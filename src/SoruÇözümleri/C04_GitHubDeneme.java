3601a92226031a68fe8e433bc99284ec09761874
Erol Balci <erolbalci3706@gmail.com>
2023-03-30 22:25:13 +0300

    deneme

---
+++ src/SoruÇözümleri/C04_GitHubDeneme.java
@@ -0,0 +1,12 @@
+package SoruÇözümleri;
+
+import java.util.Scanner;
+
+public class C04_GitHubDeneme {
+    public static void main(String[] args) {
+        Scanner scan =new Scanner(System.in);
+        System.out.println("3737");
+
+
+    }
+}
